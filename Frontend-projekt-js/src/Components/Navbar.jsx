de63e60403a8a8eca04de27d76c4c94e3ec45411
waivan <31401897+waivan@users.noreply.github.com>
2023-11-09 14:38:00 +0100

    Navbar

--- Frontend-projekt-js/src/Components/Navbar.jsx
+++ Frontend-projekt-js/src/Components/Navbar.jsx
@@ -1,26 +1,47 @@
-import {Link } from 'react-router-dom'
+import { useContext } from "react";
+import { NavLink, useLocation, useNavigate } from "react-router-dom";
+import { AuthContext } from "../context/AuthContext";
+
 
 const Navbar = () => {
+  const { updateToken, token } = useContext(AuthContext);
+
+  const location = useLocation();
+
+  const handleLogout = () => {
+    updateToken(null);
+  };
+
+  console.log(token);
+
+  const isLoginPage = location.pathname === "/login";
+
   return (
-    <div className='navbar'>
+    <div className="navbar">
       <div className="nav-part-one">
-         <div className='navtitle'>GarageBNB</div>
-    <div>
-      <Link to={'/'} className='navLink'>Parkering</Link>
-    </div>
+        <div className="navtitle">GarageBNB</div>
+        <div>
+          <NavLink to={"/"} className="NavLink">
+            Parkering
+          </NavLink>
+        </div>
       </div>
-   
-    <div className='nav-search'>
-      <input type="text" />
-      <button>Search</button>
-    </div>
 
-    <div className='nav-part-two'>
-      <Link to={'#'} className='navLink'>Mina Parkeringar</Link>
-      <Link to={'/login'} className='navLink'>Logga in</Link>
-    </div>
-    </div>
-  )
-}
+      <div className="nav-search">
+        <input type="text" />
+        <button>Search</button>
+      </div>
 
-export default Navbar
+      <div className="nav-part-two">
+        <NavLink to={"#"} className="NavLink">
+          Mina Parkeringar
+        </NavLink>
+        <NavLink to={"/login"} className="NavLink">
+          Logga in
+        </NavLink>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
